07b77688c726590fb02a3f918bf4e00d76913b98
Williams Ameyaw <williams.kyere@stu.ucc.edu.gh>
2025-07-20 21:36:08 -0400

    edit done

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -5,7 +5,7 @@ def multiplication_table():
 try:
     num = int(input("Enter a number to see its multiplication table:"))
     for i in range(1, 11):
-        print(f"{num} x {i} = {num * i}")
+        print(f"{num} * {i} = {num * i}")
 except ValueError:
     print("Please enter a valid number.")
 
